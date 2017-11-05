673146bf38dde4d3ce4e275782317c887aa92375
lesniak43 <lesniak43@gmail.com>
2017-11-05 12:49:41 +0100

    zadanie 3

--- lab/zadania/__init__.py
+++ lab/zadania/__init__.py
@@ -1,14 +1,37 @@
 import os
 
 m = __import__(os.environ['ML2018NAME'])
-__import__(os.environ['ML2018NAME'] + '.zadanie1')
-__import__(os.environ['ML2018NAME'] + '.zadanie2')
 
 # zadanie 1
-gradient_descent = m.zadanie1.gradient_descent
-momentum = m.zadanie1.momentum
-nesterov = m.zadanie1.nesterov
-adam = m.zadanie1.adam
+try:
+    __import__(os.environ['ML2018NAME'] + '.zadanie1')
+    gradient_descent = m.zadanie1.gradient_descent
+    momentum = m.zadanie1.momentum
+    nesterov = m.zadanie1.nesterov
+    adam = m.zadanie1.adam
+except ModuleNotFoundError:
+    gradient_descent = None
+    momentum = None
+    nesterov = None
+    adam = None
 
 # zadanie 2
-bias_variance_estimation = m.zadanie2.bias_variance_estimation
+try:
+    __import__(os.environ['ML2018NAME'] + '.zadanie2')
+    bias_variance_estimation = m.zadanie2.bias_variance_estimation
+except ModuleNotFoundError:
+    bias_variance_estimation = None
+
+# zadanie 3
+try:
+    __import__(os.environ['ML2018NAME'] + '.zadanie3')
+    BayesianDensityEstimator = m.zadanie3.BayesianDensityEstimator
+except ModuleNotFoundError:
+    BayesianDensityEstimator = None
+
+# zadanie 4
+try:
+    __import__(os.environ['ML2018NAME'] + '.zadanie4')
+    BayesianLinearRegression = m.zadanie4.BayesianLinearRegression
+except ModuleNotFoundError:
+    BayesianLinearRegression = None
